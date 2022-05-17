fb41d87374c5d65323118a547bf75d83cae6502d
unknown <seth.keating@gmail.com>
2022-05-17 17:41:27 -0400

    upvote query

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -62,6 +63,30 @@ router.post('/', (req, res) => {
       console.error(err);
       res.status(500).json(err);
     });
+});
+
+// PUT /api/posts/upvote
+router.put('/upvote', (req, res) => {
+  Vote.create({
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(() => {
+      return Post.findOne({
+        where: {
+          id: req.body.post_id
+        },
+        attributes: [
+          'id', 'post_url', 'title', 'created_at',
+          [
+            sequelize.literal('(select count(*) from vote where post.id = vote.post_id)'),
+            'vote_count'
+          ]
+        ]
+      })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => res.json(err));
+    })
 });
 
 // update post title
